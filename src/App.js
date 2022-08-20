3e7a7345d9ece4d5539e61fb920311b4cfb343e9
khan <omeryagci3295@gmail.com>
2022-08-20 13:53:11 +0300

    data structure created

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className>
-      <div>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-        <h1>SEa</h1>
-      </div>
+    <div className="App">
+      <div></div>
     </div>
   );
 }
---
+++ src/util/data.js
@@ -0,0 +1,38 @@
+import afrika from "../assests/afrika.png";
+import antartica from "../assests/antartica.png";
+import asia from "../assests/asia.png";
+import australia from "../assests/australia.png";
+import europe from "../assests/europe.png";
+import northAmerica from "../assests/northAmerica.png";
+import southAmerica from "../assests/southAmerica.png";
+
+export const continents = [
+  {
+    name: "Africa",
+    image: <img src={afrika} alt="africa" />,
+  },
+  {
+    name: "Antartica",
+    image: <img src={antartica} alt="antartica" />,
+  },
+  {
+    name: "Asia",
+    image: <img src={asia} alt="asia" />,
+  },
+  {
+    name: "Australia",
+    image: <img src={australia} alt="australia" />,
+  },
+  {
+    name: "Europe",
+    image: <img src={europe} alt="europe" />,
+  },
+  {
+    name: "NorthAmerica",
+    image: <img src={northAmerica} alt="northAmerica" />,
+  },
+  {
+    name: "SouthAmerica",
+    image: <img src={southAmerica} alt="southAmerica" />,
+  },
+];
